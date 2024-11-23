db24a76aa88dcc941e93d57c49de1e2c08bdc74b
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-23 16:54:03 -0500

    Add logic to display the nav menu

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createNavigationMenu } from "./navigation.js";
+
+
+
+initializeHtml();
+
+
+
+
+function initializeHtml() {
+    const body = document.querySelector('body');
+
+    body.appendChild(createNavigationMenu())
+}
