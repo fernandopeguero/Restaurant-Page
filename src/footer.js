87a44b7cef37f9f6017c65f0db3847d2fd688e9c
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-27 13:16:42 -0500

    Add footer section to code

---
+++ src/footer.js
@@ -0,0 +1,24 @@
+import { childAppender, createUnorderList } from "./util";
+
+
+export function createFooter(){
+
+    const div = document.createElement('div');
+    const section = document.createElement('section');
+    section.classList.add('container');
+
+    
+    const navigationList = createUnorderList(['Home', 'Menu', 'Reservations', 'Order Online', 'Events', 'Contact Us']);
+
+    const socialMediaList = createUnorderList(['instagram', 'Facebook', 'Twitter']);
+
+    const policyList = createUnorderList(['Privacy Policy', 'Terms & Conditions', '© 2024 Savory Delight. All Rights Reserved.'])
+
+
+    childAppender(section, navigationList, socialMediaList, policyList);
+
+    div.appendChild(section)
+
+    return div;
+
+}
