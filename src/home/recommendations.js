5d6c87ad45dc875d16329c1e9788e05451d573ed
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-26 19:59:18 -0500

    Add recommendations function to display section

---
+++ src/home/recommendations.js
@@ -0,0 +1,36 @@
+
+import steak from '../img/steak-potatoes.png';
+import { childAppender } from '../util';
+
+export function createRecommendations () {
+
+
+    const div = document.createElement('div');
+    const section = document.createElement('section');
+
+    const image = document.createElement('img');
+    image.height = 500;
+    image.width = 500;
+    image.src = steak;
+    image.alt = 'steak with vegetables';
+
+    const info = document.createElement('article');
+    info.classList.add('article');
+
+    const title = document.createElement('h2');
+    title.textContent = 'Chef’s Recommendations';
+    title.classList.add('title');
+
+    const textBody = document.createElement('p');
+    textBody.textContent = ` Indulge in our chef-curated selections, featuring innovative creations and timeless classics. Every dish is crafted to captivate your palate and elevate your dining experience`;
+    
+    childAppender(info, title, textBody);
+
+    childAppender(section, info, image)
+
+    div.appendChild(section);
+
+    return div;
+
+
+}
