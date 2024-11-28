6f44dac91d1263d2fb875590c8e1ba955a389d54
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-28 13:09:53 -0500

    Remove image on hero area
    remove the image used in the hero area and added a container
    for the title and the subtitle

--- src/home/hero.js
+++ src/home/hero.js
@@ -2,25 +2,35 @@
 Create hero area of  a section on the page home,about , others 
 */
 import lamb from '../img/lamb.png'
+import heroBackground from '../img/planing_table.jpg';
 
 import { createButton } from '../util';
 import { childAppender } from '../util';
 
+
 export function createHeroArea() {
 
     const div = document.createElement("div");
+    div.classList.add('hero_area_container');
+
     const section = document.createElement("section");
     section.classList.add('container')
 
     // Information1
     const info = document.createElement("article");
-    info.classList.add("article");
+    info.classList.add("hero_article");
+
+    const textContainer = document.createElement('div');
     
     const h1 = document.createElement("h1");
+    h1.classList.add('title')
     h1.textContent = "Where Flavor Meets Elegance"
 
     const h3 = document.createElement('h3');
+    h3.classList.add('subtitle');
     h3.textContent = "Discover the finest culinary delights in the heart of New York";
+
+    childAppender(textContainer, h1, h3);
     // Call to action 
 
     const ctaContainer = document.createElement('div');
@@ -32,15 +42,15 @@ export function createHeroArea() {
 
     childAppender(ctaContainer, viewMenu, reserveTable, orderOnline);
 
-    childAppender(info, h1 , h3 , ctaContainer)
+    childAppender(info,textContainer , ctaContainer)
 
-    const image = document.createElement("img");
-    image.height = 500;
-    image.width = 500;
-    image.src = lamb;
-    image.alt = "lamb cook to perfection"
+    // const image = document.createElement("img");
+    // image.height = 500;
+    // image.width = 500;
+    // image.src = lamb;
+    // image.alt = "lamb cook to perfection"
 
-    childAppender(section, info, image);
+    childAppender(section, info);
 
     div.appendChild(section)
     
