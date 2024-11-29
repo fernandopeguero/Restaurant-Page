fe10b10ef3eb6e7f580acadf12c1834fb625d04e
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-29 12:55:25 -0500

    Add add list for entrees/desserts of recommendations

--- src/home/recommendations.js
+++ src/home/recommendations.js
@@ -1,6 +1,6 @@
 
 import steak from '../img/plating-dish.jpg';
-import { childAppender } from '../util';
+import { childAppender, createUnorderList } from '../util';
 
 export function createRecommendations () {
 
@@ -12,13 +12,12 @@ export function createRecommendations () {
 
     const image = document.createElement('img');
     image.classList.add('recommendation_image')
-    image.height = 500;
-    image.width = 500;
+    image.width = 600;
     image.src = steak;
     image.alt = 'steak with vegetables';
 
     const info = document.createElement('article');
-    info.classList.add('article');
+    info.classList.add('recommendations_article');
 
     const title = document.createElement('h2');
     title.textContent = 'Chef’s Recommendations';
@@ -27,7 +26,27 @@ export function createRecommendations () {
     const textBody = document.createElement('p');
     textBody.textContent = ` Indulge in our chef-curated selections, featuring innovative creations and timeless classics. Every dish is crafted to captivate your palate and elevate your dining experience`;
     
-    childAppender(info, title, textBody);
+    // recommendations by the chefs list 
+
+    const recommendations = document.createElement('div');
+    recommendations.classList.add('chef_recommendations');
+
+
+    const entrees = createUnorderList(['Herb-Crusted Lamb Chops', 'Seared Halibut with Citrus Beurre Blanc', 'Filet Mignon with Truffle Butter', 'Wild Mushroom Risotto', 'Pan-Seared Duck Breast']);
+    // title entrees
+    const entreesTitle = document.createElement('h3');
+    entreesTitle.textContent = 'Entrees';
+    entrees.insertBefore(entreesTitle, entrees.firstChild);
+
+    const desserts = createUnorderList(['Molten Chocolate Lava Cake', 'Vanilla Crème Brûlée', 'Lemon Thyme Tart', 'Poached Pear in Red Wine', 'Seasonal Cheesecake'])
+    // title desserts
+    const dessertTitle = document.createElement('h3');
+    dessertTitle.textContent = 'Dessert';
+    desserts.insertBefore(dessertTitle, desserts.firstElementChild);
+    
+    childAppender(recommendations, entrees, desserts);
+
+    childAppender(info, title, textBody, recommendations);
 
     childAppender(section, info, image)
 
