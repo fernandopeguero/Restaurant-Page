ca92ad36969389aefbadc5e979e73132457ca856
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-12-02 12:03:57 -0500

    Add function for each navigation menu destination

--- src/index.js
+++ src/index.js
@@ -9,30 +9,42 @@ import { createNewsLetter } from './home/newsletter.js';
 import { createFooter } from './footer.js';
 
 
+const body = document.querySelector('body');
+// default for all pages
+const navigation = createNavigationMenu();
+const footerSection = createFooter();
+
 export function navigationController(destination) {
 
     switch(destination){
         case 'home':
-            initializeHomePage();
+            HomePage();
             break;
         case 'menu':
+            menuPage();
             break;
         case 'reservations':
+            reservationsPage();
             break;
         case 'orderOnline':
+            orderOnlinePage();
             break;
         case 'gallery':
+            galleryPage()
             break;
         case 'events':
+            eventsPage();
             break;
         case 'aboutUs':
+            aboutUsPage();
             break;
         case 'contactUs':
+            contactUsPage();
             break;
         default: 
             const body = document.querySelector('body')
 
-            body.textContent = destination;
+            body.textContent = "Page Not Found";
 
     }
 }
@@ -41,17 +53,72 @@ navigationController('home')
 
 
 
-function initializeHomePage() {
-    const body = document.querySelector('body');
+function HomePage() {
 
-    const navigation = createNavigationMenu();
+    clearPage()
+
     const heroSection = createHeroArea();
     const welcomeSection = createWelcomeSection();
     const chefRecommendations = createRecommendations();
     const testimonialsSection = createTestimonials();
     const newsletterSection = createNewsLetter();
-    const footerSection = createFooter();
+
 
     childAppender(body, navigation, heroSection, welcomeSection, chefRecommendations, testimonialsSection, newsletterSection, footerSection);
 
+}
+
+function menuPage() {
+    clearPage()
+
+
+    childAppender(body, navigation, footerSection);
+
+    
+
+}
+
+function reservationsPage() {
+    clearPage()
+
+    childAppender(body, navigation, footerSection);
+
+}
+
+function orderOnlinePage() {
+    clearPage()
+
+    childAppender(body, navigation, footerSection);
+}
+
+function galleryPage() {
+
+    clearPage()
+
+    childAppender(body, navigation, footerSection);
+}
+
+function eventsPage() {
+
+    clearPage()
+
+    childAppender(body, navigation, footerSection);
+}
+
+function aboutUsPage() {
+
+    clearPage()
+
+    childAppender(body, navigation, footerSection);
+}
+
+function contactUsPage() {
+    clearPage()
+
+    childAppender(body, navigation, footerSection);
+}
+
+function clearPage() {
+
+    body.textContent = '';
 }
