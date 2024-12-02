33ef873676bef09635026ba891185b67c1779c2d
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-12-02 09:18:51 -0500

    Update h1 text to make more legible

--- src/footer.js
+++ src/footer.js
@@ -15,7 +15,7 @@ export function createFooter(){
 
     const policyList = createUnorderList(['Privacy Policy', 'Terms & Conditions', '© 2024 Savory Delight. All Rights Reserved.'])
     
-    const author = document.createElement('h4');
+    const author = document.createElement('h3');
     author.textContent = "Create by Fernando Peguero";
 
     childAppender(section, navigationList, socialMediaList, policyList, author);
