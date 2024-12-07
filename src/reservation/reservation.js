03eb7fc64494f9eca674318ab565752b3041650e
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-12-06 23:05:14 -0500

    Add form elements to reservation.js

--- src/reservation/reservation.js
+++ src/reservation/reservation.js
@@ -1,24 +1,47 @@
-import { addOptionToSelect, childAppender, createInput } from "../util";
+import { addOptionToSelect, childAppender, createInput, createInputLabel } from "../util";
 
 
 export function createReservation() {
 
     const reservationSection = document.createElement('div');
+    reservationSection.classList.add('reservation_section');
 
     const container = document.createElement('section');
     container.classList.add('reservation_container');
 
+    const title = document.createElement('h1');
+    title.textContent = 'Reservation';
+
     // const formcontainer = document.createElement('div');
 
     const form = document.createElement('form');
-    form.classList.add('reservations-form');
+    form.classList.add('reservation_form');
 
+    // Full Name 
+    
     const fullName = createInput('full_name', 'text');
+    fullName.placeholder = 'John Doe'
+    const fullNameLabel = createInputLabel('Full Name:', 'full_name');
+
+    // Email
     const emailAddress = createInput('email_address', 'email');
+    const emailLabel = createInputLabel('Email:', 'email_address');
+    emailAddress.placeholder = 'goodfood@gmail.com'
+
+    // Phone Number
     const phoneNumber = createInput('phone_number', 'number');
+    phoneNumber.placeholder = '555-555-5555';
+    const phoneLabel = createInputLabel('Phone Number:' , 'phone_number');
+
     const reservationDate = createInput('reservation_date', 'date');
+    const reservationLabel = createInputLabel('Reservation Date:', 'reservation_date');
+
     const guestAmount = document.createElement('select');
-    const partySize = createInput('party_size', 'number');
+    guestAmount.setAttribute('id', 'number_guest');
+    guestAmount.name = 'number_guest';
+
+    const guestlabel = createInputLabel('Number Of Guest:' , 'number_guest')
+
     console.log(guestAmount)
 
     addOptionToSelect(guestAmount, '1');
@@ -28,13 +51,17 @@ export function createReservation() {
     addOptionToSelect(guestAmount, '5');
     addOptionToSelect(guestAmount, '6');
 
-    const dietaryRestrictions = createInput('special_request', 'textarea');
+    const dietaryRestrictions = document.createElement('textarea');
+    const diestaryLabel = createInputLabel('Dietary Restrictions:' , 'special_request');
 
-    childAppender(form, fullName, emailAddress, phoneNumber, reservationDate, guestAmount, partySize, dietaryRestrictions);
+    childAppender(form, title, fullNameLabel, fullName, emailLabel, emailAddress,phoneLabel, phoneNumber,reservationLabel, reservationDate,guestlabel, guestAmount,diestaryLabel, dietaryRestrictions);
 
-    container.appendChild(form)
+    const hoursContainer = document.createElement('div');
+    hoursContainer.classList.add('operation_hours');
 
-    reservationSection.appendChild(container);
+    childAppender(container, form, hoursContainer );
+
+    childAppender(reservationSection,container);
 
 
     return reservationSection;
